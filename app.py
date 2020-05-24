6747959e8881c7e459881e5725980726ba08aae1
FAROOQUE AHMED <zizo11@gmail.com>
2020-05-24 02:30:57 -0700

    Initial Commit Message

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, session
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    print(session)
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
